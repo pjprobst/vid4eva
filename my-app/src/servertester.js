f6495bf12c063147d3370af0a58d3789742cebdc
pprost <pjprobst@stu.naperville203.org>
2023-12-12 23:09:16 -0600

    af

--- my-app/src/servertester.js
+++ my-app/src/servertester.js
@@ -17,10 +17,14 @@ const mysql = require('mysql');
             console.log('Connected!')
             db.query("SELECT Title, Price, DirectorFirst, DirectorLast, OnHand FROM `Inventory`, `Branch`, `Movie`, `Director`, `Directed` WHERE Branch.BranchNum = 1 AND Branch.BranchNum = Inventory.BranchNum AND Inventory.MovieCode = Movie.MovieCode AND Movie.MovieCode = Directed.MovieCode AND Directed.DirectorNum = Director.DirectorNum;", function (err, result, fields) {
                 if (err) throw err;
-                else console.log(result),
+                else
                 strresult = JSON.stringify(result),
                 console.log(strresult),
                 db.end();
             });
         }
     })
+
+    app.listen(5000, () => {
+        console.log('Server is running on port 5000');
+      });
